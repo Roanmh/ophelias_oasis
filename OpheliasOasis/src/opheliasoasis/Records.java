e77b440f391238be4ab29fa7fb7a76385a879416
roanmh <roanmh@gmail.com>
2020-01-05 19:46:20 -0500

    Comments and Minor Refactoring
    Refactors:
     - Formatting (i.e. line breaks)
     - Method ordering

--- OpheliasOasis/src/opheliasoasis/Records.java
+++ OpheliasOasis/src/opheliasoasis/Records.java
@@ -23,28 +23,38 @@ public class Records {
     private String db_loc;
     private ArrayList<Reservation> res_db;
     private TreeMap<LocalDate, Float> rate_db;
-    // To be an absolute path to a mapped network drive on a production deployment.
+    // Part of a requirement that a separate backup be kept. Written by
+    // backup_records function stub. (Definitely a cop-out)
     private final String backup_loc = "backup.db";
-
-    public Records() {
-        this("oo.db");
-   }
 
     public ArrayList<Reservation> getResDB() {
         return res_db;
     }
 
+    /**
+     * Contructs record class with custom database location.
+     *
+     * @param dbloc location of database to use
+     */
     public Records(String db_loc) {
         this.db_loc = db_loc;
-        // Only read_in database if needed by operation.
         this.res_db = null;
         this.rate_db = null;
 
+        // Load database with extra checks
         fetch_db();
     }
 
     /**
-     * Read in database only if not already in memory.
+     * Constructs record class with default database location.
+     */
+    public Records() {
+        this("oo.db");
+    }
+
+
+    /**
+     * Read in database (only if not already in memory).
      */
     private void fetch_db() {
         if (this.res_db == null || this.rate_db == null) {
@@ -52,6 +62,12 @@ public class Records {
         }
     }
 
+    /**
+     * Finds reservations by date checked in.
+     *
+     * @param date_in check-in date to return from database
+     * @returns list of index, reservation pairs matching the date
+     */
     public List<Pair<Integer, Reservation>> lookup(LocalDate date_in) {
 
         List<Pair<Integer, Reservation>> result = new ArrayList<>();
@@ -68,6 +84,20 @@ public class Records {
         return result;
     }
 
+    /**
+     * Edits the reservation given by res_id.
+     *
+     * <p> NOTE: All parameters can be `null` for no change.
+     *
+     * @param res_id reservation id to edit
+     * @param res_type (unimplemented)
+     * @param date_in check in date of the reservation
+     * @param date_out check out date of the reservation
+     * @param name name of the reservation holder
+     * @param cc credit card of the reservation
+     * @param email of the reservation
+     * @returns changed resevation object
+     */
     public Reservation edit_reservation(int res_id,
                                         Reservation.ResType res_type,
                                         LocalDate date_in, LocalDate date_out,
@@ -97,19 +127,39 @@ public class Records {
         return res;
     }
 
+    /**
+     * Creates the reservation given by the arguments.
+     *
+     * @param res_type type of the reservation (see Reservation.ResType)
+     * @param date_in check in date of the reservation
+     * @param date_out check out date of the reservation
+     * @param name name of the reservation holder
+     * @param cc credit card of the reservation
+     * @param email of the reservation
+     * @param isChanged If this reservation is the result of a changed
+     *        reservation
+     * @returns created resevation object
+     */
     public Reservation create_reservation(Reservation.ResType res_type,
                                           LocalDate date_in, LocalDate date_out,
                                           String name,
-                                          CreditCard cc, String email, boolean isChanged) {
+                                          CreditCard cc, String email,
+                                          boolean isChanged) {
 
-        // TODO: Update for real constructor.
-        res_db.add(new Reservation(res_type,date_in, date_out, name, cc, email, isChanged));
+        res_db.add(new Reservation(res_type, date_in, date_out, name, cc, email,
+                                   isChanged));
 
         write_db();
 
         return res_db.get(res_db.size() - 1);
     }
 
+    /**
+     * Changes the base rate for a given day.
+     *
+     * @param date the date to change
+     * @param new_rate rate to set the given date to
+     */
     public void change_baseRate(LocalDate date, float new_rate) {
 
         rate_db.put(date, new_rate);
@@ -117,6 +167,11 @@ public class Records {
         write_db();
     }
 
+    /**
+     * Gets the base rate for a given day.
+     *
+     * @param date date to get
+     */
     public Float get_baseRate(LocalDate date) {
         Float baseRate;
         try {
@@ -127,9 +182,14 @@ public class Records {
             System.out.println("Base rate not set for this date " + date);
             return Float.valueOf(0);
         }
-         
+
     }
 
+    /**
+     * Notes a reservation as having checked in.
+     *
+     * @param res_id reservation id to check in
+     */
     public void check_in(int res_id) {
         Reservation reservation = getResDB().get(res_id);
         reservation.setCheckInStatus(Boolean.TRUE);
@@ -139,10 +199,16 @@ public class Records {
         write_db();
     }
 
+    /** Writes database to a backup location specified by the class. */
     public void backup_records() {
         write_db(backup_loc);
     }
 
+    /**
+     * Assigns room numbers to the reservations that check in on the given day.
+     *
+     * @param date_in check in date to assign room numbers for
+     */
     public void assignDailyRoomnumbers(LocalDate date_in){
         List<Integer> result = occupiedRoomnumbers(date_in);
         fetch_db();
@@ -156,7 +222,11 @@ public class Records {
 
     }
 
-    // gets list of roomnumbers that has been already assigned
+    /**
+     * Gets list of room numbers that have already been assigned.
+     *
+     * @param date_in date to check for reserved room numbers
+     */
     public List<Integer> occupiedRoomnumbers(LocalDate date_in){
         fetch_db();
 
@@ -174,9 +244,13 @@ public class Records {
 
     }
 
+    /**
+     * Read the database from the location specified by the object.
+     */
     private void read_db() {
         Pair<ArrayList<Reservation>, TreeMap<LocalDate, Float>> dbs;
 
+        // Using limited context input stream.
         try (FileInputStream file = new FileInputStream(db_loc);
              ObjectInputStream in = new ObjectInputStream(file)) {
 
@@ -186,16 +260,20 @@ public class Records {
             this.rate_db = dbs.getValue();
 
         } catch (IOException | ClassNotFoundException e) {
+            // Fail (Somewhat) gracefully
             out.println("Database file is unreadable or not found. Working on empty database.");
             this.res_db = new ArrayList<>();
             this.rate_db = new TreeMap<>();
         }
     }
 
-    private void write_db() {
-        write_db(db_loc);
-    }
-
+    /**
+     * Writes the database to the given location.
+     *
+     * <p>NOTE: The location parameter is included to allows use for backups.
+     *
+     * @param loc location to write the database to.
+     */
     private void write_db(String loc) {
 
         if (this.res_db == null || this.rate_db == null) {
@@ -215,6 +293,14 @@ public class Records {
         }
     }
 
+    /**
+     * Writes the database to the location sprecified by the object.
+     */
+    private void write_db() {
+        write_db(db_loc);
+    }
+
+    /** Prints to string (for debug purposes) */
     public String toString() {
         return "Records@\"%s\"(\n" + this.res_db.toString() + "\n" + this.rate_db.toString() + "\n)\n";
     }
